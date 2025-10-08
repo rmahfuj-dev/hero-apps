475f2ee83c488ac51b39be2c50a5bfe529c81cda
rmahfuj-dev <rmahfujur782@gmail.com>
2025-10-08 15:21:51 +0600

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Layout from './layouts/Layout'
 
 const App = () => {
   return (
-    <div>App</div>
+   <Layout />
   )
 }
 
---
+++ src/router.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children:[{}]
+  },
+]);
+
+export default router;
